97098471beb79f42a35237d04d6ac1d7e8bda210
HarivonyR <harivonyratefiarison@gmail.com>
2025-07-13 11:46:41 +0300

    ADD bs4 & selector

--- main.py
+++ main.py
@@ -5,14 +5,29 @@ Created on Sun Jul 13 11:03:05 2025
 @author: harivonyratefiarison
 """
 
+
+# Fetch HTML data
+
+
 from credential import x_api_key
 import requests
 
 url = "https://piloterr.com/api/v2/website/crawler"
 
 headers = {"x-api-key": x_api_key}
+querystring = {"query":"https://www.reddit.com/r/announcements/"}
 
-response = requests.request("GET", url, headers=headers)
+response = requests.request("GET", url, headers=headers,params=querystring)
 
 print(response.text)
 
+
+# Extract data (subject/topic)
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# selector : document.querySelectorAll('[slot="full-post-link"]')
+# postTitle = document.querySelectorAll('[slot="full-post-link"]')[5].innerText
+
+all_posts = soup.select('[slot="full-post-link"]')
