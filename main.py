882878d2cbdcc7ac30f4eeab760fd2cf2aa5465e
HarivonyR <harivonyratefiarison@gmail.com>
2025-07-16 23:12:22 +0300

    ADD post select and info print loop

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ Topic Link : https://www.reddit.com/topics/a-1/
 #----------------------
 from script.crawler import crawl
 
-sample_topics = "https://www.reddit.com/t/american_top_team/"
-response = crawl(query=sample_topics)
+topics_link = "https://www.reddit.com/t/american_top_team/"
+response = crawl(query=topics_link)
+
 
 # Decode raw html
 #----------------------------
@@ -31,14 +32,24 @@ soup = BeautifulSoup(clean_html, 'html.parser')
 # Select all posts
 #----------------------------
 articles = soup.select('article')
+#print(articles)
 
 # get post details
 for article in articles :
-
+    #article = articles[0]   # get a sample for the test
     article_title = article["aria-label"]
-    article_link = article.find('shreddit-post')['content-href']
     article_author = article.find("shreddit-post")["author"]
+    article_link = article.find('a',href=True)["href"]
+    article_date = article.find('shreddit-post')["created-timestamp"]
+    article_total_comment = article.find('shreddit-post')["comment-count"]
+    article_score = article.find('shreddit-post')["score"]
     
-    print(f"{article_title} : {article_link}")
+    print(f"date : {article_date}")
+    print(f"author : {article_author}")
+    print(f"score : {article_score}")
+    print(f"title : {article_title}")
+    print(f"relative link : {article_link}")
+    print(f"total comment : {article_total_comment}")
+    
     print("-----------------------")
 
